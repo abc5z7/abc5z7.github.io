30dc808100e7fdcc2fd26bad19a2e923b5c2c058
abc5z7 <abcwzcb@163.com>
2025-04-01 22:21:18 +0800

    Site updated: 2025-04-01 22:21:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/01/hello-world/","2025/04/01/lolcat/"];function toRandomPost(){
+var posts=["2025/04/01/hello-world/","2023/07/24/美化Mac终端/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
