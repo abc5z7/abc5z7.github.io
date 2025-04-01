f62fa02621303e4998fced0f8f727bf0aeaf1437
abc5z7 <abcwzcb@163.com>
2025-04-01 16:04:14 +0800

    Site updated: 2025-04-01 16:04:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/01/lolcat/","2025/04/01/hello-world/"];function toRandomPost(){
+var posts=["2025/04/01/hello-world/","2025/04/01/lolcat/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
