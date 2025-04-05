af3e7b6b78c30ad2388f4061e79652fdc10a8a3d
abc5z7 <abcwzcb@163.com>
2025-04-05 22:04:18 +0800

    Site updated: 2025-04-05 22:04:18

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/01/pycharm打字卡顿/","2024/12/02/safebrowsing偷跑流量/","2023/07/23/lolcat报错/","2020/04/04/todolist/","2023/07/24/美化Mac终端/","2023/07/24/自定义ohmyzsh/"];function toRandomPost(){
+var posts=["2023/07/23/lolcat报错/","2024/07/01/pycharm打字卡顿/","2024/12/02/safebrowsing偷跑流量/","2020/04/04/todolist/","2023/07/24/美化Mac终端/","2023/07/24/自定义ohmyzsh/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
